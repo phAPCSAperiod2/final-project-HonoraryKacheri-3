ed0622f47183e4279d7b09d4111055521342e7c4
Tristan P. <tristanrpatterson@gmail.com>
2026-05-01 10:43:10 -0700

    Rename PokemonTrainer to PokemonTeam and add strategy
    Refactor PokemonTrainer class to PokemonTeam and add strategy methods.

--- src/PokemonTeam.java
+++ src/PokemonTeam.java
@@ -1,12 +1,15 @@
-public class PokemonTrainer
+import Java.util.Scanner;
+public class PokemonTeam
 {
+    Scanner input = new Scanner(System.in);
     // private constants
     private static final int MAX_POKEMON = 6;
-    private String trainerName = name;
+    private String partyName = name;
     private ArrayList<Pokemon> party;
+    private String generalStrategy = "";
 
-    public PokemonTrainer(String name){
-        this.trainerName = name;
+    public PokemonTeam(String name){
+        this.partyName = name;
         this.party = new ArrayList<Pokemon>();
     }
 
@@ -18,5 +21,14 @@ public class PokemonTrainer
         else{
             return false;
         }
+    }
+
+    public void addStrategy();{
+        System.out.println("Input your team's general strategy.");
+        generalStrategy = input.nextLine();
+    }
+
+    public void interPokeStrategy(Pokemon p1, Pokemon p2){
+        System.out.println("What is the strategy between these two Pokemon?");
     }
 }
