90a9e2aa74a14e627453ed7286ae7475708fc4eb
Tristan P. <tristanrpatterson@gmail.com>
2026-05-06 17:07:50 +0000

    modified:   bin/PokemonTeam.class 	modified:   src/Pokemon.java 	modified:   src/PokemonTeam.java 	modified:   bin/Pokemon.class 	modified:   bin/PokemonTeam.class

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class Pokemon
-{
+
+public class Pokemon {
     // Private constants
     Scanner input = new Scanner(System.in);
     private static final int MAX_HEALTH = 500;
     private static final int MAX_MOVES = 4;
 
     private String name;
-    private String type;
+    private String type1;
+    private String type2;
     private int hp;
     private int atk;
     private int def;
@@ -19,7 +20,8 @@ public class Pokemon
     private String roleDesc;
     private String strategyDesc;
 
-    public Pokemon(String pokemonName, String type1, String type2, int hp, int atk, int def, int spa, int spd, int spe){
+    public Pokemon(String pokemonName, String type1, String type2, int hp, int atk, int def, int spa, int spd,
+            int spe) {
         this.name = pokemonName;
         this.hp = hp;
         this.atk = atk;
@@ -30,17 +32,19 @@ public class Pokemon
         this.movesList = new ArrayList<Moves>();
         String strategy = "";
         this.strategyDesc = strategy;
+        this.type1 = type1;
+        this.type2 = type2;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getHealth(){
+    public int getHealth() {
         return hp;
     }
 
-    public boolean learnMove(){
+    public boolean learnMove() {
         System.out.print("What will be the move's name? ");
         String moveName = input.nextLine();
         System.out.println();
@@ -49,25 +53,24 @@ public class Pokemon
         System.out.println();
         System.out.print("How much damage does it do? ");
         int moveDamage = input.nextInt();
-        if (moveDamage == 0){
+        if (moveDamage == 0) {
             System.out.println("What does the move do?");
             String moveDesc = input.nextLine();
-            System.out.println("It will be named " + moveName + ", be " + moveType + "type, and it will " + moveDesc + ".");
-            if (movesList.size() >= 4){
+            System.out.println(
+                    "It will be named " + moveName + ", be " + moveType + "type, and it will " + moveDesc + ".");
+            if (movesList.size() >= MAX_MOVES) {
                 return false;
-            }
-            else{
+            } else {
                 movesList.add(Moves(moveName, moveType, moveDesc));
                 return true;
             }
-        }
-        else{
+        } else {
             System.out.println();
-            System.out.println("It will be named " + moveName + ", be " + moveType + " type, and deal " + moveDamage + " damage.");
-            if (movesList.size() >= 4){
+            System.out.println(
+                    "It will be named " + moveName + ", be " + moveType + " type, and deal " + moveDamage + " damage.");
+            if (movesList.size() >= 4) {
                 return false;
-            }
-            else{
+            } else {
                 movesList.add(Moves(moveName, moveType, moveDamage));
                 return true;
             }
@@ -82,69 +85,73 @@ public class Pokemon
         throw new UnsupportedOperationException("Unimplemented method 'Moves'");
     }
 
-    public int getHP(){
+    public int getHP() {
         return this.hp;
     }
 
-    public int getATK(){
+    public int getATK() {
         return this.atk;
     }
 
-    public int getDEF(){
+    public int getDEF() {
         return this.def;
     }
 
-    public int getSPA(){
+    public int getSPA() {
         return this.spa;
     }
 
-    public int getSPD(){
+    public int getSPD() {
         return this.spd;
     }
 
-    public int getSPE(){
+    public int getSPE() {
         return this.spe;
     }
 
-    public String getRole(){
+    public String getRole() {
         return this.roleDesc;
     }
 
-    public String getStrategy(){
+    public String getStrategy() {
         return this.strategyDesc;
     }
 
-    public boolean canLearnMoreMoves(){
-        if (movesList.size() >= 4){
+    public String getTypes() {
+        return ("This Pokemon is a " + type1 + " type, and a " + type2 + " type.");
+    }
+
+    public boolean canLearnMoreMoves() {
+        if (movesList.size() >= 4) {
             return false;
-        }
-        else{
+        } else {
             return true;
         }
     }
 
-    public void forgetMove(int i){
+    public void forgetMove(int i) {
         movesList.remove(i);
     }
 
-    public void addDescription(){
+    public void addDescription() {
         System.out.println("Describe what your Pokemon should do, what role it should fill.");
         String roleDesc = input.nextLine();
-        System.out.println("Describe what strategies your Pokemon would do, such as move sequences or synergies/counters with other Pokemon.");
+        System.out.println(
+                "Describe what strategies your Pokemon would do, such as move sequences or synergies/counters with other Pokemon.");
         String strategyDesc = input.nextLine();
         this.roleDesc = roleDesc;
         this.strategyDesc = strategyDesc;
     }
 
-    public String toString(){
+    public String toString() {
         return (getName() + " (Health: " + hp + " / " + MAX_HEALTH + ")");
     }
 
-    public ArrayList<Moves> getMoves(){
+    public ArrayList<Moves> getMoves() {
         return movesList;
     }
 
-    public boolean knowsMove(Moves move){
+    public boolean knowsMove(Moves move) {
         return movesList.contains(move);
     }
 }
--- src/PokemonTeam.java
+++ src/PokemonTeam.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class PokemonTeam
-{
+
+public class PokemonTeam {
     Scanner input = new Scanner(System.in);
     // private constants
     private static final int MAX_POKEMON = 6;
@@ -9,23 +9,33 @@ public class PokemonTeam
     private ArrayList<Pokemon> party;
     private String generalStrategy = "";
 
-    public PokemonTeam(String name){
+    public PokemonTeam(String name) {
         this.partyName = name;
         this.party = new ArrayList<Pokemon>();
     }
 
-    public boolean addPokemon(Pokemon p){
-        if (party.size() >= 6){
+    public boolean addPokemon(Pokemon p) {
+        if (party.size() >= MAX_POKEMON) {
             party.add(p);
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public void addStrategy(){
+    public void addStrategy() {
         System.out.println("Input your team's general strategy.");
         generalStrategy = input.nextLine();
+    }
+
+    public String toString() {
+        return ("The team is named " + partyName + ", has " + party.size()
+                + " Pokemon inside it, and the general strategy is " + generalStrategy);
+    }
+
+    public void pokemonList() {
+        for (int i = 0; i <= party.size(); i++) {
+            System.out.println("Name: " + party.get(i).getName() + ", types: " + party.get(i).getTypes() + ".");
+        }
     }
 }
